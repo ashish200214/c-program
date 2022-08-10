b303ddfda60050ef5cf4c4452d5e66b9cf1736c9
ashish200214 <64855416+ashish200214@users.noreply.github.com>
2022-08-10 19:52:15 +0530

    Update structure.c

--- structure.c
+++ structure.c
@@ -1,6 +1,21 @@
 #include<stdio.h>
-#include<conio.h>
+#include<string.h>
+struct student
+{
+ char name[100];
+ int roll;
+ float mark;
+};
+void printinfo(struct student s1);
 int main()
 {
- printf("hello world");
+ struct student s1;
+ strcpy(s1.name,"Ashish");
+ s1.roll=12;
+ s1.mark=75.7;
+ printinfo(s1); 
+}
+void printinfo(struct student s1)
+{
+ printf("Name of student is %s\nRoll number is %d\nMarks is %f",s1.name,s1.roll,s1.mark);
 }
