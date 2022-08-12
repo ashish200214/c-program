ed6d82f6c2befc4844c4b860410cfa8fdaede88b
ashish200214 <64855416+ashish200214@users.noreply.github.com>
2022-08-12 13:28:07 +0530

    Create lower_upper_letter.c

---
+++ lower_upper_letter.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int no;
+	char ch;
+	printf("enter the letter in lower case\n");
+	scanf("%c",&ch);
+	no=ch-32;
+	printf("The letter in upper is %c",no);
+	return 0;
+}
