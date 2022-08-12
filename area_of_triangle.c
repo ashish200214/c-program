1ff90efc19bd3b1ee3daba2e17e89e60e98a3c9b
ashish200214 <64855416+ashish200214@users.noreply.github.com>
2022-08-12 13:13:38 +0530

    Create area_of_triangle.c

---
+++ area_of_triangle.c
@@ -0,0 +1,15 @@
+
+//area of triangle 
+#include<stdio.h>
+
+int main()
+{
+int length,breadth,area;
+printf("enter the length and breadth");
+scanf("%d%d",&length,&breadth);
+area=(length*breadth)/2;
+printf("area of triangle is %d",area);
+
+ return 0;
+ 
+}
