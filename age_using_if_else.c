7c38bdb979c04cc84e7b24e2793e0b55791065a8
ashish200214 <64855416+ashish200214@users.noreply.github.com>
2022-08-14 11:34:53 +0530

    Create age_using_if_else.c

---
+++ age_using_if_else.c
@@ -0,0 +1,20 @@
+
+// Age using if-else
+#include<stdio.h>
+
+int main()
+{
+    int age;
+    printf("enter your age");
+    scanf("%d",&age);
+    if(age>=18)
+    {
+    	printf("eligible for voting");
+	}
+	else
+	{
+		printf("not eligible for voting");
+	}
+    
+    return 0;
+}
