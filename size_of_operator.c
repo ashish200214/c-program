77138d7352576f9b43798ebfbbaebdf001b9b764
ashish200214 <64855416+ashish200214@users.noreply.github.com>
2022-08-14 11:31:43 +0530

    Create size_of_operator.c

---
+++ size_of_operator.c
@@ -0,0 +1,12 @@
+// size of operator
+
+#include<stdio.h>
+
+int main()
+{
+    int a=55;
+    char ch='x';
+    printf("size of integer is %d \n",sizeof(a));
+    printf("size of character is %d",sizeof(ch));
+    return 0;
+}
