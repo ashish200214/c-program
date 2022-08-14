ff57e106becaba87e856f7366722b479540d58bb
ashish200214 <64855416+ashish200214@users.noreply.github.com>
2022-08-14 16:55:00 +0530

    Update table_using_function.c

--- table_using_function.c
+++ table_using_function.c
@@ -21,3 +21,23 @@ table(n);
  return 0;
  
 }
+
+
+
+
+// do while loop table number///
+#include <stdio.h>
+int main()
+{
+int n;
+printf("enter value of n");
+scanf("%d",&n);
+int i=1;
+do{
+	printf("%d\n",i*n);
+	i++;
+}
+while(i<=10);
+return 0;
+
+}
