926ac4b54f9a56e4e179962d1e514d177ac1f54b
ashish200214 <64855416+ashish200214@users.noreply.github.com>
2022-08-14 12:51:05 +0530

    Create patter.c

---
+++ patter.c
@@ -0,0 +1,23 @@
+
+// pattern
+// *
+// **
+// ***
+// ****
+// *****
+#include <stdio.h>
+int main()
+{
+   for(int i=1;i<=5;i++)
+   {
+   	for(int j=1;j<=i;j++)
+   	{
+   		printf("*");
+	}
+	printf("\n");
+	   
+   }
+   
+   return 0;
+
+}
