6e84671b37c1c81cf39272ffee24251b65c6890c
ashish200214 <64855416+ashish200214@users.noreply.github.com>
2022-08-14 11:30:51 +0530

    Create Asci_value_finder.c

---
+++ Asci_value_finder.c
@@ -0,0 +1,15 @@
+
+// Ascii value
+
+#include<stdio.h>
+
+int main()
+{
+    char ch;
+    
+    printf("enter the character");
+    scanf("%c",&ch);
+    printf("ascii value is %d",ch);
+    
+    return 0;
+}
