46ecccc7425040da58dba94e1359b9766b229ba3
ashish200214 <64855416+ashish200214@users.noreply.github.com>
2022-08-14 13:27:45 +0530

    Create pattern_new.c

---
+++ pattern_new.c
@@ -0,0 +1,27 @@
+
+// pattern
+//  *****
+//   ****
+//    ***
+//     **
+//      *
+#include <stdio.h>
+int main()
+{
+   for(int i=1;i<=5;i++)   //for row
+   {
+   	for(int j=1;j<i;j++)   //for space 
+   	{
+   		printf(" ");
+	}
+	for (int k=i;k<=5;k++) //for column   
+	{
+		printf("*");
+	}
+	printf("\n");
+	   
+   }
+   
+   return 0;
+
+}
