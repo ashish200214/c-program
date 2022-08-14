b6bd6b8929f165129ebe59c34730c994aa9cd915
ashish200214 <64855416+ashish200214@users.noreply.github.com>
2022-08-14 11:32:47 +0530

    Create age_using_ternary.c

---
+++ age_using_ternary.c
@@ -0,0 +1,13 @@
+
+// Age using ternary Operator
+#include<stdio.h>
+
+int main()
+{
+    int age;
+    printf("enter your age");
+    scanf("%d",&age);
+    (age>=18)?printf("eligible for voting"):printf("not eligible for voting");
+    
+    return 0;
+}
