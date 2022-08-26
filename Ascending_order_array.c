1014d7c23adfb0a69121b51268536750fa3e82d3
ashish200214 <64855416+ashish200214@users.noreply.github.com>
2022-08-26 22:49:20 +0530

    Create Ascending_order_array.c

---
+++ Ascending_order_array.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+	int a[5],i,j,temp;
+	printf("enter the elements\n");
+	for(i=0;i<5;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<5;i++)
+	{
+		for(j=i+1;j<5;j++)
+		{
+			if(a[i]>a[j])
+			{
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+				
+			}
+		}
+	}
+	printf("Ascending of number is :\n");
+	for(i=0;i<5;i++)
+	{
+		printf("%d ",a[i]);
+	}
+	
+}
