756937b27a48795788f234364ffec3b33f824175
ashish200214 <64855416+ashish200214@users.noreply.github.com>
2022-09-19 19:22:16 +0530

    Create even_no_in_reverse.c

---
+++ even_no_in_reverse.c
@@ -0,0 +1,17 @@
+//Printing Even Number in reverse order
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	printf("enter the number\n");
+	scanf("%d",&n);
+	i=n;
+	while(i>=1)
+	{
+	     if(i%2==0)
+	     {
+	     	printf("%d\n",i);
+		 }
+		 i--;
+	}
+}
